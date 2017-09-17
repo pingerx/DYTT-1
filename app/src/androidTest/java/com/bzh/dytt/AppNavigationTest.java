e703717a8fdea1f50adb03312cd636f9f5f911ff
biezhihua <biezhihua@gmail.com>
2017-09-17 09:18:29 +0800

    [Feature] First Commit & Create Empty Project

---
+++ app/src/androidTest/java/com/bzh/dytt/AppNavigationTest.java
@@ -0,0 +1,51 @@
+package com.bzh.dytt;
+
+import android.app.Activity;
+import android.support.annotation.IdRes;
+import android.support.annotation.NonNull;
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.Gravity;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.contrib.DrawerMatchers.isClosed;
+import static android.support.test.espresso.contrib.DrawerMatchers.isOpen;
+import static android.support.test.espresso.matcher.ViewMatchers.withContentDescription;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+
+@RunWith(AndroidJUnit4.class)
+public class AppNavigationTest {
+
+    @Rule
+    public ActivityTestRule<MainActivity> mActivityTestRule = new ActivityTestRule<MainActivity>(MainActivity.class);
+
+    @Test
+    public void clickHomeIconToOpenNavigation() {
+        onView(withId(R.id.drawer_layout)).check(matches(isClosed(Gravity.LEFT)));
+
+        onView(withContentDescription(getToolbarNavigationContentDescription(mActivityTestRule.getActivity(), R.id.toolbar))).perform(click());
+
+        onView(withId(R.id.drawer_layout)).check(matches(isOpen(Gravity.LEFT)));
+    }
+
+
+    /**
+     * Returns the content description for the navigation button view in the toolbar.
+     */
+    public static String getToolbarNavigationContentDescription(@NonNull Activity activity, @IdRes int toolbar1) {
+        Toolbar toolbar = (Toolbar) activity.findViewById(toolbar1);
+        if (toolbar != null) {
+            return (String) toolbar.getNavigationContentDescription();
+        } else {
+            throw new RuntimeException("No toolbar found.");
+        }
+    }
+}
---
+++ app/src/main/java/com/bzh/dytt/MainFragment.java
@@ -0,0 +1,24 @@
+package com.bzh.dytt;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class MainFragment extends Fragment {
+
+    public static MainFragment newInstance() {
+        return new MainFragment();
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+
+        View root = inflater.inflate(R.layout.content_main, container, false);
+
+        return root;
+    }
+}
