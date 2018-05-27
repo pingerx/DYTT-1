f9da141cf620fc18a47e11dae36e4cc3bfd24718
biezhihua <biezhihua@gmail.com>
2018-05-27 16:18:13 +0800

    Remove UI Test

--- app/src/main/java/com/bzh/dytt/di/FragmentBuildersModule.java
+++ app/src/main/java/com/bzh/dytt/di/FragmentBuildersModule.java
@@ -2,8 +2,6 @@ package com.bzh.dytt.di;
 
 import com.bzh.dytt.search.SearchFragment;
 import com.bzh.dytt.ui.AllMoviePageFragment;
-import com.bzh.dytt.ui.ColorHuntFragment;
-import com.bzh.dytt.ui.GirlFragment;
 import com.bzh.dytt.ui.ImdbMovieFragment;
 import com.bzh.dytt.ui.LoadableMoviePageFragment;
 import com.bzh.dytt.ui.NewMovieFragment;
@@ -17,12 +15,6 @@ public abstract class FragmentBuildersModule {
 
     @ContributesAndroidInjector
     abstract AllMoviePageFragment contributeHomePageFragment();
-
-    @ContributesAndroidInjector
-    abstract ColorHuntFragment contributeColorHuntFragment();
-
-    @ContributesAndroidInjector
-    abstract GirlFragment contributeGirlFragment();
 
     @ContributesAndroidInjector
     abstract VideoDetailPageFragment contributeVideoDetailFragment();
