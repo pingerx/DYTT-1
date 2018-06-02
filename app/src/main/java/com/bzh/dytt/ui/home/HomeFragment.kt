49109102d20314545d356e08907c13ce2a7db16e
biezhihua <biezhihua@gmail.com>
2018-06-02 19:05:37 +0800

    [Feature] Tab Data

--- app/src/main/java/com/bzh/dytt/ui/home/HomeFragment.kt
+++ app/src/main/java/com/bzh/dytt/ui/home/HomeFragment.kt
@@ -1,14 +1,21 @@
 package com.bzh.dytt.ui.home
 
+import android.arch.lifecycle.Observer
 import android.arch.lifecycle.ViewModelProvider
 import android.arch.lifecycle.ViewModelProviders
+import android.content.res.Resources
 import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.support.v4.app.FragmentManager
+import android.support.v4.app.FragmentPagerAdapter
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.bzh.dytt.R
 import com.bzh.dytt.base.BaseFragment
 import com.bzh.dytt.di.Injectable
+import com.bzh.dytt.ui.NewMovieFragment
+import kotlinx.android.synthetic.main.home_fragment.*
 import javax.inject.Inject
 
 class HomeFragment : BaseFragment(), Injectable {
@@ -18,6 +25,18 @@ class HomeFragment : BaseFragment(), Injectable {
 
     private lateinit var homeViewModel: HomeViewModel
 
+    private lateinit var homePagerAdapter: HomePagerAdapter
+
+    private var tabObserver: Observer<List<HomeViewModel.HomeMovieType>> = Observer { tabs ->
+        if (tabs != null) {
+            homePagerAdapter.data.clear()
+            for (tab in tabs) {
+                homePagerAdapter.data.add(tab)
+            }
+            homePagerAdapter.notifyDataSetChanged()
+        }
+    }
+
     override fun doCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
         return inflater.inflate(R.layout.home_fragment, container, false)
     }
@@ -26,9 +45,14 @@ class HomeFragment : BaseFragment(), Injectable {
         super.doViewCreated(view, savedInstanceState)
         homeViewModel = ViewModelProviders.of(this, viewModelFactory).get(HomeViewModel::class.java)
         lifecycle.addObserver(homeViewModel)
+        homeViewModel.tabLiveData.observe(this, tabObserver)
+        home_tab_layout.setupWithViewPager(home_view_pager)
+        homePagerAdapter = HomePagerAdapter(resources, fragmentManager)
+        home_view_pager.adapter = homePagerAdapter
     }
 
     override fun doDestroyView() {
+        homeViewModel.tabLiveData.removeObserver(tabObserver)
         lifecycle.removeObserver(homeViewModel)
         super.doDestroyView()
     }
@@ -37,5 +61,51 @@ class HomeFragment : BaseFragment(), Injectable {
         fun newInstance(): HomeFragment {
             return HomeFragment()
         }
+    }
+
+    class HomePagerAdapter(private val resources: Resources, private val fragmentManager: FragmentManager?) : FragmentPagerAdapter(fragmentManager) {
+
+        val data = arrayListOf<HomeViewModel.HomeMovieType>()
+
+        override fun getPageTitle(position: Int): String = when (data[position]) {
+            HomeViewModel.HomeMovieType.MOVIE_ZUIXIN_FILM -> {
+                resources.getString(R.string.home_tab_item_zuixin_film)
+            }
+            HomeViewModel.HomeMovieType.MOVIE_ZONGHE_FILM -> {
+                resources.getString(R.string.home_tab_item_zonghe_film)
+            }
+            HomeViewModel.HomeMovieType.MOVIE_HUAYU_FILM -> {
+                resources.getString(R.string.home_tab_item_huayu_film)
+            }
+            HomeViewModel.HomeMovieType.MOIVE_OUMEI_FILM -> {
+                resources.getString(R.string.home_tab_item_oumei_film)
+            }
+            HomeViewModel.HomeMovieType.MOIVE_RIHAN_FILM -> {
+                resources.getString(R.string.home_tab_item_rihan_film)
+            }
+            HomeViewModel.HomeMovieType.MOVIE_HUAYU_TV -> {
+                resources.getString(R.string.home_tab_item_huayu_tv)
+            }
+            HomeViewModel.HomeMovieType.MOVIE_OUTMEI_TV -> {
+                resources.getString(R.string.home_tab_item_oumei_tv)
+            }
+            HomeViewModel.HomeMovieType.MOVIE_RIHAN_TV -> {
+                resources.getString(R.string.home_tab_item_rihan_tv)
+            }
+            HomeViewModel.HomeMovieType.MOVIE_ZENGYIV -> {
+                resources.getString(R.string.home_tab_item_zongyi)
+            }
+            else -> {
+                resources.getString(R.string.home_tab_item_zuixin_film)
+            }
+        }
+
+        override fun getItem(position: Int): Fragment {
+//            data[position]
+            return NewMovieFragment.newInstance()
+        }
+
+        override fun getCount() = data.size
+
     }
 }
--- app/src/main/java/com/bzh/dytt/ui/home/HomeViewModel.kt
+++ app/src/main/java/com/bzh/dytt/ui/home/HomeViewModel.kt
@@ -1,26 +1,51 @@
 package com.bzh.dytt.ui.home
 
-import android.arch.lifecycle.Lifecycle
-import android.arch.lifecycle.LifecycleObserver
-import android.arch.lifecycle.OnLifecycleEvent
-import android.arch.lifecycle.ViewModel
-import android.util.Log
+import android.arch.lifecycle.*
 import com.bzh.dytt.repository.DataRepository
 import javax.inject.Inject
 
 class HomeViewModel @Inject constructor(dataRepository: DataRepository) : ViewModel(), LifecycleObserver {
 
+    private val mTabLiveData: MutableLiveData<List<HomeMovieType>> = MutableLiveData<List<HomeMovieType>>()
+
+    val tabLiveData: LiveData<List<HomeMovieType>>
+        get() = mTabLiveData
+
+
     @OnLifecycleEvent(Lifecycle.Event.ON_RESUME)
     fun active() {
-        Log.d(TAG, "active")
+        mTabLiveData.value = arrayListOf(
+                HomeMovieType.MOVIE_ZUIXIN_FILM,
+                HomeMovieType.MOVIE_ZONGHE_FILM,
+                HomeMovieType.MOVIE_HUAYU_FILM,
+                HomeMovieType.MOIVE_OUMEI_FILM,
+                HomeMovieType.MOIVE_RIHAN_FILM,
+                HomeMovieType.MOVIE_HUAYU_TV,
+                HomeMovieType.MOVIE_OUTMEI_TV,
+                HomeMovieType.MOVIE_RIHAN_TV,
+                HomeMovieType.MOVIE_ZENGYIV,
+                HomeMovieType.MOVIE_DONGMAN
+        )
     }
 
     @OnLifecycleEvent(Lifecycle.Event.ON_PAUSE)
     fun inactive() {
-        Log.d(TAG, "inactive")
     }
 
     companion object {
         const val TAG = "HomeViewModel"
+    }
+
+    enum class HomeMovieType(val type: Int) {
+        MOVIE_ZUIXIN_FILM(9),
+        MOVIE_ZONGHE_FILM(10),
+        MOVIE_HUAYU_FILM(1),
+        MOIVE_OUMEI_FILM(2),
+        MOIVE_RIHAN_FILM(3),
+        MOVIE_HUAYU_TV(4),
+        MOVIE_OUTMEI_TV(5),
+        MOVIE_RIHAN_TV(6),
+        MOVIE_ZENGYIV(7),
+        MOVIE_DONGMAN(8);
     }
 }
